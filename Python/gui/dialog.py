d3ed047f13bbf7790388e85743035bbc1b70b088
yhkim-4504 <ky45042@naver.com>
2021-05-12 19:38:04 +0900

    Update dialog.py

--- Python/gui/dialog.py
+++ Python/gui/dialog.py
@@ -3,17 +3,17 @@ from PyQt5.QtWidgets import QLabel, QGridLayout, QPushButton, QDialog
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5 import QtWebEngineWidgets
 
-temp_url = "https://www.youtube.com/embed/t67_zAg5vvI?autoplay=1"
+temp_url = "https://www.youtube.com/embed/HtQmxoWytIA"
 
 class YoutubeDialog(QDialog):
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.setWindowTitle('Dialog Title')
+        self.setWindowTitle('스트레칭 영상')
         self.setWindowModality(Qt.ApplicationModal)
 
         self.webview = QtWebEngineWidgets.QWebEngineView(self)
         self.webview.setUrl(QUrl(temp_url))
-        self.webview.setGeometry(0, 0, 500, 300)
+        self.webview.setGeometry(0, 0, 900, 600)
 
     def set_webview_url(self, url: str) -> None:
         self.webview.setUrl(QUrl(url))
@@ -26,12 +26,12 @@ class YoutubeDialog(QDialog):
 class GuideDialog(QDialog):
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.setWindowTitle('Guide Dialog')
+        self.setWindowTitle('스트레칭 알림')
         self.setWindowModality(Qt.ApplicationModal)
 
-        self.label = QLabel('여실래요?')
-        self.yes_btn = QPushButton('네')
-        self.no_btn = QPushButton('아니요')
+        self.label = QLabel('여시겠습니까?')
+        self.yes_btn = QPushButton('네!')
+        self.no_btn = QPushButton('아니요..')
 
         layout = QGridLayout()
         layout.addWidget(self.label, 0, 0, 2, 4)
