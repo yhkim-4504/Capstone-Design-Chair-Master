ebfeeeac1ff791f594689774a5ed18e2b764a1d0
yhkim-4504 <ky45042@naver.com>
2021-05-20 23:01:14 +0900

    Set DarkStyleSheet

--- run_gui.py
+++ run_gui.py
@@ -1,8 +1,11 @@
 import sys
+import qdarkstyle
 from Python.gui_app import GuiApp
 from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     gui_app = GuiApp()
+    dark_stylesheet = qdarkstyle.load_stylesheet_pyqt5()
+    app.setStyleSheet(dark_stylesheet)
     sys.exit(app.exec_())
